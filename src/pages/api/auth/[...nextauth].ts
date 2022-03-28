5ba9233a912084bd982eca050a66d5902f6237c2
Marcelo3duardo <fmarcelo3duardo@gmail.com>
2022-03-28 11:01:59 -0300

    save a  user in BD

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as qry } from 'faunadb';
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -18,4 +22,26 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  /*jwt: {
+    signingKey: process.env.JWT_KEY,
+  },*/
+  callbacks: {
+    async signIn({ user}) {
+      console.log(user);
+     const { email } = user
+      try {
+        await fauna.query(
+          qry.Create(
+            qry.Collection('user'),
+            { data: { email } }
+  
+          )
+        )
+        return true
+      } catch  {
+        return false
+      }
+     
+    }
+  }
 })
